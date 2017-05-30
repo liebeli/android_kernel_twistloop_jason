45e6c5bf760ad58a33ece71cc1217f0edab67313
Chris Redpath <chris.redpath@arm.com>
2017-05-30 14:51:53 +0100

    BACKPORT: sched/fair: Make it possible to account fair load avg consistently
    While set_task_rq_fair() is introduced in mainline by commit ad936d8658fd
    ("sched/fair: Make it possible to account fair load avg consistently"),
    the function results to be introduced here by the backport of
    commit 09a43ace1f98 ("sched/fair: Propagate load during synchronous
    attach/detach"). The problem (apart from the confusion introduced by the
    backport) is actually that set_task_rq_fair() is currently not called at
    all.
    
    Fix the problem by backporting again commit ad936d8658fd
    ("sched/fair: Make it possible to account fair load avg consistently").
    
    Original change log:
    
    The current code accounts for the time a task was absent from the fair
    class (per ATTACH_AGE_LOAD). However it does not work correctly when a
    task got migrated or moved to another cgroup while outside of the fair
    class.
    
    This patch tries to address that by aging on migration. We locklessly
    read the 'last_update_time' stamp from both the old and new cfs_rq,
    ages the load upto the old time, and sets it to the new time.
    
    These timestamps should in general not be more than 1 tick apart from
    one another, so there is a definite bound on things.
    
    Signed-off-by: Byungchul Park <byungchul.park@lge.com>
    [ Changelog, a few edits and !SMP build fix ]
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1445616981-29904-2-git-send-email-byungchul.park@lge.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry-picked from ad936d8658fd348338cb7d42c577dac77892b074)
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Change-Id: I17294ab0ada3901d35895014715fd60952949358
    Signed-off-by: Brendan Jackman <brendan.jackman@arm.com>
    
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2237,6 +2237,10 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	init_task_runtime_info(p);
 #endif
 
+#ifdef CONFIG_FAIR_GROUP_SCHED
+	p->se.cfs_rq			= NULL;
+#endif
+
 #ifdef CONFIG_SCHEDSTATS
 	memset(&p->se.statistics, 0, sizeof(p->se.statistics));
 #endif
